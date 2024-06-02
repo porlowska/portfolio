7fab05575496d1b587369ed7370d54885f74103e
Polly Orlowska <polly.orlowska@gmail.com>
2024-06-02 15:55:35 +0100

    responsive design for About me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,30 +3,33 @@ import Image from "next/image";
 const AboutMe = () => {
   return (
     <section className="bg-black text-white p-10">
-      <h2 className=" text-center font-heading text-3xl mb-4">
+      <h2 className=" text-center font-heading text-5xl mb-4 p-4">
         About <span className="text-secondary">Me</span>
       </h2>
       <div className="flex flex-col md:flex-row justify-evenly">
-        <div className="flex-1">
-          <Image
+        <div>
+          
+          <Image 
+          className="rounded-full m-auto p-3"
             src="/pic/portrait.jpg"
-            width={540}
-            height={540}
+            width={340}
+            height={340}
             alt="Picture of Polly"
           />
           <Image
+          className="rounded p-3 m-auto"
             src="/pic/certificate.png"
-            width={632}
-            height={474}
+            width={500}
+            height={375}
             alt="Certificate Front end web development"
           />
         </div>
-        <div className="flex-1">
-          <p className="font-body">
+        <div className=" flex flex-col flex-1 p-3 max-w-[500px] justify-center text-center">
+          <p className="font-body pb-6 pt-6">
             I’m Polly Orlowska, a front-end web developer and engeneer based in
             London.
           </p>
-          <p className="font-body">
+          <p className="font-body  pb-6">
             I am skilled in HTML, CSS, JavaScript, and React JS & NextJS,
             seeking a position to advance these skills in a dynamic development
             role. My background includes launching a startup and leading
@@ -35,7 +38,7 @@ const AboutMe = () => {
             user-centric solutions that drive business success.
           </p>
 
-          <p className="font-body">
+          <p className="font-body pb-12">
           If you’re looking for someone who understands the heartbeat of a business and can translate it into a compelling web presence, you’ve found the right person.
           </p>
           <p className="font-body">
@@ -44,6 +47,11 @@ const AboutMe = () => {
             Tools:Git, GitHub Web Design:Wix Website Builder, SEO, UX/UI Design,
             Responsive Web Design
           </p>
+
+          <video className="w-[200px] m-auto"  autoPlay loop preload="auto">
+        <source src="/videos/tabs.mp4" type="video/mp4" />
+        Your browser does not support videos.
+      </video>
 
         </div>
       </div>
