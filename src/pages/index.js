238a3315e05885b33cfd8c9af0bdd89490d818cf
Polly Orlowska <polly.orlowska@gmail.com>
2024-06-02 22:48:31 +0100

    netlify deployment debugging - NavBar error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 // src/pages/index.js
-import NavBar from '../components/NavBar';
+// import NavBar from '../components/NavBar';
 import Welcome from '../components/Welcome';
 import AboutMe from '../components/AboutMe';
 import Projects from '../components/Projects';
@@ -9,7 +9,7 @@ import Footer from '../components/Footer';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <NavBar />
+      {/* <NavBar /> */}
       <main className="flex-grow">
         <Welcome />
         <div id="about">
